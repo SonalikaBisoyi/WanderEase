374c6ff8768d67d343cdca4353185eab3c9d4974
SonalikaBisoyi <125640005+SonalikaBisoyi@users.noreply.github.com>
2023-08-20 11:07:03 +0530

    Update customerReviews.js

--- backend/models/customerReviews.js
+++ backend/models/customerReviews.js
@@ -24,9 +24,7 @@ module.exports = (sequelize) => {
     },
 
     }, {
-    timestamps: false, // This disables the automatic timestamp columns
+    timestamps: false;
   });
-  console.log("458");
-  console.log(CustomerReview);
   return CustomerReview;
 };
