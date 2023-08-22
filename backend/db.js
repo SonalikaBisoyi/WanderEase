379cba7c60278b37a44f631c0afd8b9325e9dafa
SonalikaBisoyi <125640005+SonalikaBisoyi@users.noreply.github.com>
2023-08-23 00:16:40 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,27 +1,13 @@
 
 const { Sequelize } = require('sequelize');
-const config = require('../backend/config/config.json'); 
-const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
-  config.development
-);
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Database connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
-const defineCustomerReviewModel = require('./models/customerReviews');
-const CustomerReview = defineCustomerReviewModel(sequelize);
-const defineUserModel = require('./models/user');
-const User = defineUserModel(sequelize);
 
-module.exports = {
-  sequelize,
-  CustomerReview,
-  User,
-};
+const sequelize = new Sequelize({
+  
+  dialect: 'mysql', 
+  host: 'localhost', 
+  username: 'root', 
+  password: 'root@24', 
+  database: 'wanderease', 
+});
+
+module.exports = { sequelize };
