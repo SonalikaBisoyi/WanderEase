9cbe228272d4fe1cc2968986e6a72a5d6c9f7f5e
SonalikaBisoyi <125640005+SonalikaBisoyi@users.noreply.github.com>
2023-08-27 04:49:49 +0530

    Update Contactus.jsx

--- frontend/src/Pages/Contactus.jsx
+++ frontend/src/Pages/Contactus.jsx
@@ -8,10 +8,10 @@ const Contact = () => {
       bgImage="https://img.freepik.com/free-photo/top-view-chat-bubbles-with-telephone-receiver-copy-space_23-2148796078.jpg?w=826&t=st=1692480421~exp=1692481021~hmac=747bdf56c99da89ad5b78f9103ca2bfb4896bb8e38971b0debd009278ca59ff1"
       bgSize="cover"
     >
-      <Container marginTop="6rem" textAlign="center">
-      <Text fontSize="6xl">Feel Free to Contact us</Text>
+      <Text fontSize="6xl" textAlign="center">Feel Free to Contact us</Text>
 
         <Flex justifyContent="space-between">
+        
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15126.28620995241!2d73.92422475000001!3d18.59334505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c14df5c70e0d%3A0x2d19689e09e2fced!2sPhoenix%20Mall%20Washrooms!5e0!3m2!1sen!2sin!4v1658905192255!5m2!1sen!2sin"
             width="100%"
@@ -20,7 +20,9 @@ const Contact = () => {
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"></iframe>
-          <Box width="100%">
+
+      
+          <Box width="100%" alignItems="center">
             <Container className="contact-form">
               <form
                 action="https://formspree.io/f/mbjvolad"
@@ -44,17 +46,18 @@ const Contact = () => {
 
                 <Textarea
                   name="message"
+                  placeholder="TYPE YOUR MESSAGE HERE..."
                   rows="6"
                   autoComplete="off"
                   required
                 ></Textarea>
 
-                <Button type="submit">send</Button>
+                <Button  type="submit">send</Button>
               </form>
             </Container>
           </Box>
         </Flex>
-      </Container>
+    
     </Box>
   );
 };
