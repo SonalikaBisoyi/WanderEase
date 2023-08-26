c939496d5b41c8419a901cb6887b978e53519f2d
Abrar101102 <141958499+Abrar101102@users.noreply.github.com>
2023-08-26 16:39:13 +0530

    Update RegisterPage.jsx

--- frontend/src/Pages/RegisterPage.jsx
+++ frontend/src/Pages/RegisterPage.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from 'react';
-import { Box, Input, Button } from '@chakra-ui/react';
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  Link,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {Link} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
-function RegisterPage() {
+export default function RegisterPage() {
+  const [showPassword, setShowPassword] = useState(false);
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [regsuccess, setRegsuccess] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,42 +44,97 @@ function RegisterPage() {
         body: JSON.stringify({ username, email, password }),
       });
 
+      setRegsuccess(true);
+
       const data = await response.json();
       console.log('User registered:', data);
       toast.success('User registered successfully!');
-      return <Link to="/" />;
     } catch (error) {
       console.error('Registration error:', error);
       toast.error('An error occurred. Please try again later.');
     }
   };
 
-  return (
-    <Box p={4}>
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button type="submit">Register</Button>
-      </form>
-    </Box>
-  );
-}
+  if (regsuccess) {
+    return <Navigate to="/login" />;
+  }
 
-export default RegisterPage;
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={'white.800'}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'} textAlign={'center'}>
+            Sign up
+          </Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool features ✌
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={'gray.700'}
+          boxShadow={'lg'}
+          p={8}>
+          <form onSubmit={handleSubmit}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>UserName</FormLabel>
+                  <Input type="text" onChange={(e) => setUsername(e.target.value)} />
+                </FormControl>
+              </Box>
+              {/* <Box>
+                <FormControl id="lastName">
+                  <FormLabel>Last Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box> */}
+            </HStack>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" onChange={(e) => setEmail(e.target.value)} />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={showPassword ? 'text' : 'password'}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}
+                type="submit">
+                Sign up
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={'center'}>
+                Already a user? <Link color={'blue.400'}>Login</Link>
+              </Text>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
