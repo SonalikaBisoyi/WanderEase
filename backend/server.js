640d62373e2a8c72b9d5ca459ee0965aa32e7c63
SonalikaBisoyi <125640005+SonalikaBisoyi@users.noreply.github.com>
2023-08-27 04:55:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
+
 const express = require('express');
 const cors = require('cors');
 const reviewsRoutes = require('./routes/reviews'); 
-const authRoutes = require('./routes/auth.js'); 
+const authRoutes = require('./routes/auth.js');
+const stateCityRoutes = require('./routes/stateCityRoutes');
+const siteRoutes = require('./routes/siteRoutes');
+const popularSiteRoutes = require('./routes/popularSiteRoutes');
+const singleSiteRoutes = require('./routes/singleSiteRoutes');
 const { sequelize } = require('./db'); 
-
+const db=require('./models')
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,12 +17,19 @@ app.use(express.urlencoded({ extended: true }));
   await sequelize.sync();
   console.log('Models synchronized with the database.');
 })();
-app.use('/api', reviewsRoutes);
 app.use('/api', authRoutes);
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use('/api', reviewsRoutes);
+app.use('/api', stateCityRoutes);
+app.use('/api/sites', siteRoutes);
+//app.use('/agents', agentRoutes);
+//app.use('/site-agents', siteAgentRoutes);
+app.use('/api', popularSiteRoutes);
+//app.use('/api', popularSiteRoutes, singleSiteRoutes);
+//const PORT = process.env.PORT || 3001;
+db.sequelize.sync().then(()=>{
+app.listen(3001, () => {
+  console.log(`Server is running on port 3001`);
+});
 });
 
 
